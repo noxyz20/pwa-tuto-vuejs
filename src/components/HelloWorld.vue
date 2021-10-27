cd2a04d3f85c274ba792bc637f157aafd21207ad
franck <contact@franckcolonna.fr>
2021-10-27 15:04:19 +0200

    Partie 6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,23 @@
 <template>
-   <span> Hello {{ message }} </span>
+   <span> Hello {{ message }} </span><br/> 
+   <span> {{ OnlyFirstWord }} </span><br/> 
    <span v-if="estVisible"> Mon Texte </span>
-
 </template>
 <script>
   export default {
      data() {
        return {
-          message: "World",
+          message: "Zoulette World",
           compteur: 13,
           pourcentage: 60,
           estVisible: true,
           tableauVide: []
        }
+    },
+    computed: {
+      OnlyFirstWord() {
+        return this.message.split(' ')[0].toUpperCase();
+      }
     }
   }
 </script>
